4bd77f19bdda31daf3313f04beb6cab09b5bf0eb
idumea <idumea@bitdefender.com>
2017-10-19 10:31:08 +0300

    25th_commit

--- _new_file.py
+++ _new_file.py
@@ -1,9 +1,11 @@
 import os
 import blabla
 
+print("this is just de begining")
+
 os.chdir('/home/idumea/disk/tests/')
 os.system('cd {}'.format(os.getcwd()))
 os.system('ls')
 
 print(blabla.polinom2(3, 1, 2, 3))
-print(end)
+print("end")
